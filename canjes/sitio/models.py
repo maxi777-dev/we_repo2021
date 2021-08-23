4fbc8f39209b7f47772aa336ff8dc40854ee55e4
Maximiliano Mainero <maxx321xx@gmail.com>
2021-08-23 10:01:03 -0300

    fix profile

--- canjes/sitio/models.py
+++ canjes/sitio/models.py
@@ -1,4 +1,4 @@
-from django.contrib.auth.models import User
+"""from django.contrib.auth.models import User
 from django.db import models
 from django.db.models.signals import post_save
 from django.dispatch import receiver
@@ -24,4 +24,20 @@ class Profile(models.Model):
 def create_or_update_user_profile(sender, instance, created, **kwargs):
     if created:
         Profile.objects.create(user=instance)
-    instance.profile.save()
+    instance.profile.save()"""
+
+  
+from django.db import models
+from django.contrib.auth.models import User
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete = models.CASCADE)
+    name = models.CharField(max_length = 40, null = True)
+    lastname = models.CharField(max_length = 40, null = True)
+    location = models.CharField(max_length = 40, null = True)
+    cp = models.IntegerField(max_length = 40, null = True)
+    fecha_nacimiento = models.DateField(max_length = 40, null = True)
+    cuil_cuit = models.CharField(max_length = 40, null = True)
+
+    def __str__(self):
+        return self.nombre + ' ' + self.apellido
