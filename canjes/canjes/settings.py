bd0357040116ebcf17db39eafb882174fd00a104
Maximiliano Mainero <maxx321xx@gmail.com>
2021-08-25 15:40:54 -0300

    test change password ._.

--- canjes/canjes/settings.py
+++ canjes/canjes/settings.py
@@ -134,7 +134,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'validate.canjea@gmail.com'
-EMAIL_HOST_PASSWORD = 'U2ZgE[hkp7WdvKF?'
+EMAIL_HOST_PASSWORD = 'jajaja xD'
 EMAIL_USE_TLS = True
 
 django_heroku.settings(locals())
