f43d464c5c41a16fa1204b0e1a9a3521ed142674
DiegoBressan <diegocarpbressan@gmail.com>
2021-10-12 17:57:55 -0300

    robots.txt configurado.

--- canjes/canjes/urls.py
+++ canjes/canjes/urls.py
@@ -16,6 +16,7 @@ Including another URLconf
 from os import name
 from django.contrib import admin
 from django.urls import path
+from django.views.generic.base import TemplateView
 from sitio import views
 from django.conf import settings
 from django.conf.urls.static import static
